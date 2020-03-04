e3206ef548a5d7cf6539926b65a3b12db1cfc973
KavyaRajendran <kavyarrajendran01@gmail.com>
2020-03-04 17:43:04 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-  //var prom_pt = prompt("Please enter your name");
- // var start = prompt("enter value");
-  //alert("output : "+prom_pt.slice(start,end));
+ /* var prom_pt = prompt("Please enter your name");
+  var start = prompt("enter start value");
+  
   if(isNaN(start))
   {
-    alert("enter start value");
+    var start = prompt("Enter Start value");
+    var end = prompt("Enter end value");
+    alert("output:"+prom_pt.slice(start,end));
+
   }
   else{
     var end = prompt("Enter end value");
-  }
+    alert("output : "+prom_pt.slice(start,end));
+  }*/
   
 
 
