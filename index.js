32b7bbbd820aa07b1dc69b433b42d771bcc7b6ce
KavyaRajendran <kavyarrajendran01@gmail.com>
2020-03-04 17:44:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// Import stylesheets
-import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
  /* var prom_pt = prompt("Please enter your name");
   var start = prompt("enter start value");
