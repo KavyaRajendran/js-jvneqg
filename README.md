2138467ce7a758435aff6383ade45e8eda5cb553
KavyaRajendran <kavyarrajendran01@gmail.com>
2020-03-04 17:32:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jvneqg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jvneqg)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+  //var prom_pt = prompt("Please enter your name");
+ // var start = prompt("enter value");
+  //alert("output : "+prom_pt.slice(start,end));
+  if(isNaN(start))
+  {
+    alert("enter start value");
+  }
+  else{
+    var end = prompt("Enter end value");
+  }
+  
+
+
+ 
